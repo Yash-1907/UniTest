7113a08cae0bbc19670c28bc5c62343db0e1509f
Yash <ybhanushali1902@gmail.com>
2022-05-15 17:55:34 +0530

    first

---
+++ hardhat.config.js
@@ -0,0 +1,50 @@
+require("@nomiclabs/hardhat-waffle");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+
+const ALCHEMY_API_KEY = "c609e01d3136427e8dd8ae2adea198aa";
+const RINKEBY_PRIVATE_KEY = "0x743a6a63199d341463f8cf6544778af8de8655faaaacd3d81b9074564a280c07";
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.7.6",
+  networks: {
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${ALCHEMY_API_KEY}`,
+      accounts: [`${RINKEBY_PRIVATE_KEY}`]
+    },
+    kovan: {
+      url: `https://kovan.infura.io/v3/77fec7e08400400f949aee7b1cbd5b7c`,
+      accounts: [`${RINKEBY_PRIVATE_KEY}`]
+    }
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: 'Z4B8XEHDDWJ9X93GI41W199R81C1URAR78'
+  },
+
+
+  solidity: {
+    version: "0.7.6",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+}
